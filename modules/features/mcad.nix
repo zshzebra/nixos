5868b646cb8faf08229b227007afdb13e9fd39d9
zshzebra <ryder@retzlaff.family>
2026-07-27 11:16:05 +1000

    feat: remove uneeded overlay for freecad

--- modules/features/mcad.nix
+++ modules/features/mcad.nix
@@ -6,36 +6,5 @@
       environment.systemPackages = with pkgs; [
         freecad
       ];
-
-      # Fix until nixpkgs pr #537721 get's merged
-      nixpkgs.overlays = [
-        (final: prev: {
-          pdal = prev.pdal.overrideAttrs (old: rec {
-            version = "2.10.0";
-            src = prev.fetchFromGitHub {
-              owner = "PDAL";
-              repo = "PDAL";
-              tag = version;
-              hash = "sha256-uqWawto3EJJaFhmhQn9eg+4s7NuhmVO5YXC6igkCeU0=";
-            };
-            disabledTests = (old.disabledTests or [ ]) ++ [
-              "pdal_io_copc_remote_reader_test"
-            ];
-          });
-
-          vtk_9_5 = prev.vtk_9_5.overrideAttrs (old: {
-            NIX_CFLAGS_COMPILE = (old.NIX_CFLAGS_COMPILE or "") + " -fpermissive";
-          });
-
-          freecad = prev.freecad.overrideAttrs (old: {
-            postInstall = ''
-              sed -i \
-                -e "s|^TryExec=.*freecad-thumbnailer.*|TryExec=$out/bin/freecad-thumbnailer|" \
-                -e "s|^Exec=.*freecad-thumbnailer|Exec=$out/bin/freecad-thumbnailer|" \
-                $out/share/thumbnailers/FreeCAD.thumbnailer
-            '';
-          });
-        })
-      ];
     };
 }
