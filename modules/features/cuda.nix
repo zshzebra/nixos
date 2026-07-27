a18eb9d8d22c68ead6c04dd64b0cf57c99965a8a
zshzebra <ryder@retzlaff.family>
2026-07-27 16:14:15 +1000

    revert: back out cmake 4.3 cuda patch and flake update

--- modules/features/cuda.nix
+++ modules/features/cuda.nix
@@ -13,35 +13,5 @@
       };
 
       nixpkgs.config.cudaSupport = true;
-
-      # Apply fix in nixpkgs pr #545542
-      nixpkgs.overlays = [
-        (
-          final: prev:
-          let
-            cudaPatch = prev.fetchpatch {
-              name = "cuda-setup-hook-nvcc-545542.patch";
-              url = "https://github.com/NixOS/nixpkgs/pull/545542.diff?full_index=1";
-              hash = "sha256-5U2DTTLvvCOTQJd3wTJIzL/1HFdX0u7w4XGKTOAR0cw=";
-            };
-            patchScope =
-              cudaPkgs:
-              cudaPkgs.overrideScope (
-                cudaFinal: cudaPrev: {
-                  setupCudaHook = cudaPrev.setupCudaHook.overrideAttrs (old: {
-                    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.patch ];
-                    buildCommand = old.buildCommand + ''
-                      patch -p1 "$out/nix-support/setup-hook" < ${cudaPatch}
-                    '';
-                  });
-                }
-              );
-            needsPatch = prev.lib.versionAtLeast prev.cmake.version "4.3";
-          in
-          prev.lib.genAttrs [ "cudaPackages_12" "cudaPackages_13" ] (
-            name: if needsPatch then patchScope prev.${name} else prev.${name}
-          )
-        )
-      ];
     };
 }
