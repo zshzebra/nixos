56dbc07c1ae4eba6258bdf4b1adb1fe6891d5e68
zshzebra <ryder@retzlaff.family>
2026-07-27 16:51:41 +1000

    fix: remove NVIDIA prime whilst iGPU is unused

--- modules/hosts/workstation/nvidia.nix
+++ modules/hosts/workstation/nvidia.nix
@@ -3,21 +3,9 @@
   flake.nixosModules.workstationNvidia =
     { ... }:
     {
-      # Ensure iGPU for display
-      services.xserver.videoDrivers = [ "amdgpu" ];
-
       hardware.nvidia = {
         # TODO: Evaluate if power management is an issue
         powerManagement.enable = true;
-
-        prime = {
-          offload = {
-            enable = true;
-            enableOffloadCmd = true;
-          };
-          amdgpuBusId = "PCI:122:0:0";
-          nvidiaBusId = "PCI:1:0:0";
-        };
       };
     };
 }
