0e394c6a98ff6d67a9728805f1ae6b7313ebdf8c
zshzebra <ryder@retzlaff.family>
2026-09-18 16:14:11 +1000

    fix: use newer NVIDIA driver

--- modules/features/nvidia.nix
+++ modules/features/nvidia.nix
@@ -20,7 +20,16 @@
       hardware.nvidia = {
         open = true;
 
-        package = config.boot.kernelPackages.nvidiaPackages.latest;
+        # NOTE: Use newer NVIDIA driver than 26.05 nixpkgs, until nixpkgs is updated
+        # package = config.boot.kernelPackages.nvidiaPackages.latest;
+        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "595.91.07";
+          sha256_64bit = "sha256-yiPIjdJLB6GRZE4eEc+3vN11NzBXSa9A+YABiwleYxM=";
+          sha256_aarch64 = "sha256-fqkN7ONFXtTeXyu2mQxorrk362Epxq3bz88hhKYQzwQ=";
+          openSha256 = "sha256-OB8Epd+qn/WywxsPiFpxEOAzlJqb6I1SyRoV3a8l71k=";
+          settingsSha256 = "sha256-QzT8Cw1luuZGP9DUje3HN/0ngiayqHURj+bqPsxlJ5w=";
+          persistencedSha256 = "sha256-3JQBaNmkwxvCXv9q8aHKas6VZM/JjLsuilC2t7ET0u0=";
+        };
 
         modesetting.enable = true;
       };
